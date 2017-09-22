7c3233e1a80657f74c37224a4b2e7a2ef6ec7543
jmazrimas <joe.mazrimas@gmail.com>
2017-09-21 20:04:29 -0500

    have location factory working as expected

--- scripts/rocketWeatherController.js
+++ scripts/rocketWeatherController.js
@@ -1,30 +1,26 @@
+"use strict";
+
 angular.module("rocketWeather")
-.controller("rocketWeatherController", function($scope) {
+.controller("rocketWeatherController", function($scope, $http, locationFactory) {
 
     $scope.testFunc = function() {
       return 999;
     }
 
-    var getLocation = function() {
-      if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(showPosition, showError);
-      } else {
-          console.log("Geolocation is not supported by this browser.");
+    var defaultLocation = {latitude: 41.885383, longitude: -87.644481}
+    $scope.locationSearchInProgress = true;
+
+    locationFactory.getUserLocation().then(
+      function(location){
+        console.log(location)
+        $scope.locationSearchInProgress = false;
+        $scope.location = location;
+      },
+      function(error){
+        $scope.locationSearchInProgress = false;
+        $scope.location = defaultLocation;
+        $scope.error=error;
       }
-    }
+    )
 
-    function showPosition(position) {
-      console.log("Latitude: " + position.coords.latitude);
-      console.log("Longitude: " + position.coords.longitude);
-    }
-
-    function showError(error) {
-      if (error.PERMISSION_DENIED) {
-        console.log("user denied");
-      } else {
-        console.log("other error")
-      }
-    }
-
-    getLocation();
 });
